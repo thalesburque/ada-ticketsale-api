ec1517a8f107ec8463ac73c5df74e5b8e451f257
Johny Maia <johnymaiamonteiro@gmail.com>
2023-03-08 19:29:34 -0300

    Configurações do Eureka.

--- src/main/java/com/ada/ticketsaleapi/AdaTicketsaleApiApplication.java
+++ src/main/java/com/ada/ticketsaleapi/AdaTicketsaleApiApplication.java
@@ -2,8 +2,10 @@ package com.ada.ticketsaleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdaTicketsaleApiApplication {
 
 	public static void main(String[] args) {
